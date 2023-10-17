40efa86dc769dfafba0d76babb46d27f638f4b33
gibson <gb.lee.gibson@gmail.com>
2023-10-17 17:35:43 +0900

    Fix Get API
    Remove Body and replace it with query parameters

--- internal/project/handler/handlerTokenInfo.go
+++ internal/project/handler/handlerTokenInfo.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"log"
 	"net/http"
+	"time"
 	"token-assignment/internal/project/dto"
 
 	"github.com/labstack/echo/v4"
@@ -10,11 +11,29 @@ import (
 
 func (h *ProjectHandler) GetTokenInfo(c echo.Context) error {
 	var err error
-	var dtoReq dto.GetTokenInfoReq
 
-	if err = c.Bind(&dtoReq); err != nil {
-		log.Println(err)
-		return c.JSON(http.StatusOK, dto.Response{ErrorMessage: "Invalid Body"})
+	symbolStr := c.QueryParam("tokenSymbol")
+	sourceStr := c.QueryParam("source")
+	startTimeStr := c.QueryParam("startTime")
+	endTimeStr := c.QueryParam("endTime")
+
+	dtoReq := dto.GetTokenInfoReq{
+		Symbol: symbolStr,
+		Source: sourceStr,
+	}
+
+	if startTimeStr != "" {
+		startTime, err := time.Parse(time.RFC3339, startTimeStr)
+		if err != nil {
+			return c.JSON(http.StatusOK, dto.Response{ErrorMessage: "Invalid time format"})
+		}
+		dtoReq.StartTime = startTime
+
+		endTime, err := time.Parse(time.RFC3339, endTimeStr)
+		if err != nil {
+			return c.JSON(http.StatusOK, dto.Response{ErrorMessage: "Invalid time format"})
+		}
+		dtoReq.EndTime = endTime
 	}
 
 	if err = c.Validate(dtoReq); err != nil {
